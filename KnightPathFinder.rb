06b8f4dda19c8de05677bcaa254d0f684c1261a9
Justin <jcdiner@gmail.com>
2023-01-30 10:57:15 -0500

    started PolyTreeNode

---
+++ KnightPathFinder.rb
@@ -0,0 +1,9 @@
+class KnightPathFinder
+	
+	def initialize(position)
+		 @position = position
+		
+	end
+end
+
+kpf = KnightPathFinder.new([0, 0])
---
+++ PolyTreeNode.rb
@@ -0,0 +1,12 @@
+class PolyTreeNode
+	attr_accessor :position, :neighbors
+
+	def initialize(position)
+		self.position = position
+		self.neighbors = []
+	end
+
+	def self.populate
+		
+	end
+end
